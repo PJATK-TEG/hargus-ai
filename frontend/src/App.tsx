05bb4c5a03d7c4cbb832f8b7ab0da4d348f4728a
s28312 <s28312@pjwstk.edu.pl>
2026-04-16 14:11:06 +0200

    Frontend: add utils + missing pages/routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
+import AnalyticsPage from './pages/AnalyticsPage'
+import CandidatesPage from './pages/CandidatesPage'
+import SettingsPage from './pages/SettingsPage'
 import VacanciesPage from './pages/VacanciesPage'
 import VacancyDetailPage from './pages/VacancyDetailPage'
 import CandidateDetailPage from './pages/CandidateDetailPage'
@@ -10,6 +13,9 @@ export default function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Navigate to="/vacancies" replace />} />
         <Route path="/vacancies" element={<VacanciesPage />} />
+        <Route path="/candidates" element={<CandidatesPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
         <Route path="/vacancies/:vacancyId" element={<VacancyDetailPage />} />
         <Route path="/vacancies/:vacancyId/candidates/:candidateId" element={<CandidateDetailPage />} />
       </Route>
